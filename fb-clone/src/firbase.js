d58ddd043886dd8b952faa5db3461c3ab8116d40
samueltiokeng <samueledward40@gmail.com>
2020-09-22 10:06:21 +0200

    f

---
+++ fb-clone/src/firbase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAK1JGGp3W-0vtVBmtIIH-yHw_WVxYJS9M",
+    authDomain: "facebook-clone-c2329.firebaseapp.com",
+    databaseURL: "https://facebook-clone-c2329.firebaseio.com",
+    projectId: "facebook-clone-c2329",
+    storageBucket: "facebook-clone-c2329.appspot.com",
+    messagingSenderId: "640552925033",
+    appId: "1:640552925033:web:0de275e8ec5056ff5deff8",
+    measurementId: "G-KQBSRHQLQ3"
+  };
