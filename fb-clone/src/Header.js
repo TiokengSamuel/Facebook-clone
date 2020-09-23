4528f3baeb7cd2aca0eb6c00993813e576a921de
samueltiokeng <samueledward40@gmail.com>
2020-09-23 07:08:06 +0200

    icon

--- fb-clone/src/Header.js
+++ fb-clone/src/Header.js
@@ -3,8 +3,9 @@ import './Header.css'
 import SearchIcon from '@material-ui/icons/Search'
 import HomeIcon from '@material-ui/icons/Home'
 import FlagIcon from '@material-ui/icons/Flag'
-import SubscriptionsOutlined from '@material-ui/icons/SubscriptionsOutlined'
+import SubscriptionsOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined'
 import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined'
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle'
 
 
 
