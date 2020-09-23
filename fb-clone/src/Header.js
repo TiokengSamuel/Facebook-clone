1a3da78d8104f46cb45466b2c1cc039aaa1033e2
samueltiokeng <samueledward40@gmail.com>
2020-09-23 06:38:13 +0200

    h

--- fb-clone/src/Header.js
+++ fb-clone/src/Header.js
@@ -7,13 +7,15 @@ function Header() {
         <div className="header">
             <div className="header__left">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png" alt=""/>
-
-            </div>
-            
-            <div className="header__middle">
                 <div className="header__input">
                     <SearchIcon/>
                     <input type="text" />
+                </div>
+            </div>
+            
+            <div className="header__middle">
+                <div className="header__option">
+                    <HomeIcon fontSize="large" />
                 </div>
             </div>
        
