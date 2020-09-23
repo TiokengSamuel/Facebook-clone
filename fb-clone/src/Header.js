751850074302d92bf3a04bff69b60130a6f47672
samueltiokeng <samueledward40@gmail.com>
2020-09-23 05:55:11 +0200

    svg

--- fb-clone/src/Header.js
+++ fb-clone/src/Header.js
@@ -5,7 +5,7 @@ function Header() {
     return (
         <div className="header">
             <div className="header__left">
-                <img src="https://upload.wikimedia.org" alt=""/>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png" alt=""/>
 
             </div>
             
