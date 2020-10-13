3b0bb9dc25af8d2af0b19d2120a866a7b4b212b7
TiokengSamuel <samueledward40@gmail.com>
2020-10-13 05:18:27 +0100

    s

--- fb-clone/src/Story.js
+++ fb-clone/src/Story.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Story.css'
 
-function Story({props}) {
+function Story({ image, profileSrc, title}) {
     return (
         <div className="story">
             
