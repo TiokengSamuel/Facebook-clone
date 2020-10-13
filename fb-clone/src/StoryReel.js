c526fb6b9b3277950d5cfeb655d3a5f8db4d20c4
TiokengSamuel <samueledward40@gmail.com>
2020-10-13 09:46:14 +0100

    sR

--- fb-clone/src/StoryReel.js
+++ fb-clone/src/StoryReel.js
@@ -17,8 +17,8 @@ function StoryReel() {
             />
             <Story
             image=""
-            profileSrc=""
-            title=""
+            profileSrc="https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ed560d07fe4060006bbce1e%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D422%26cropX2%3D1300%26cropY1%3D0%26cropY2%3D879"
+            title="Ellen DeGeneres"
             />
             <Story
             image=""
