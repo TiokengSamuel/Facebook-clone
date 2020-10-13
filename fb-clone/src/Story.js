5cc2837744b0cac63c28c4f961c120b5cf45bc2b
TiokengSamuel <samueledward40@gmail.com>
2020-10-13 10:08:53 +0100

    s

--- fb-clone/src/Story.js
+++ fb-clone/src/Story.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import './Story.css'
+import {Avatar} from "@material-ui/core"
 
 
 function Story({ image, profileSrc, title}) {
     return (
         <div className="story">
-            <Avatar src={profileSrc}/>
+            <Avatar/>
             <h4>{title}</h4>
         </div>
     )
 }
+
 
 export default Story
