97afdf718b73b0d8ec35f8b3d0addfbff13ec0b9
TiokengSamuel <samueledward40@gmail.com>
2020-10-13 10:17:48 +0100

    s

--- fb-clone/src/Story.js
+++ fb-clone/src/Story.js
@@ -5,7 +5,7 @@ import {Avatar} from "@material-ui/core"
 
 function Story({ image, profileSrc, title}) {
     return (
-        <div className="story">
+        <div style={} className="story">
             <Avatar src={profileSrc}/>
             <h4>{title}</h4>
         </div>
