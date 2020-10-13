9b29b56d17523de23575adb3bc5d0724990e0332
TiokengSamuel <samueledward40@gmail.com>
2020-10-13 22:01:27 +0100

    f

--- fb-clone/src/Feed.js
+++ fb-clone/src/Feed.js
@@ -14,7 +14,7 @@ function Feed() {
                 message="Wow it worked"
                 timestamp="This is a timestamp"
                 username="Tiokeng Samuel"
-                image={image}
+                image="https://bucket.mg.co.za/wp-media/2019/06/f5a37fb1-00-yogan-pillay-south-africa-aids-conference-new-infections-innovations-696x445.jpeg"
             />
         </div>
     )
