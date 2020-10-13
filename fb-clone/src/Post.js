c338fde24c01bb7d96288a783a10fcafa0ba020a
TiokengSamuel <samueledward40@gmail.com>
2020-10-13 21:17:34 +0100

    post

--- fb-clone/src/Post.js
+++ fb-clone/src/Post.js
@@ -9,6 +9,9 @@ function Post({ profilePic, image, username, timestamp, message}) {
                 <Avatar src={profilePic}
                 className="post__avatar"
                 />
+                <div className="post__topInfo">
+                    <h3>{}</h3>
+                </div>
 
             </div>
         </div>
