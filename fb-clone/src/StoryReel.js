7052a8d7695f67fb535eecdc263a656a39e87490
TiokengSamuel <samueledward40@gmail.com>
2020-10-13 07:16:41 +0100

    story

--- fb-clone/src/StoryReel.js
+++ fb-clone/src/StoryReel.js
@@ -6,9 +6,11 @@ function StoryReel() {
     return (
         <div className="storyReel">
             <Story
-            image=""
+            image="https://bucket.mg.co.za/wp-media/2019/06/f5a37fb1-00-yogan-pillay-south-africa-aids-conference-new-infections-innovations-696x445.jpeg"
             profileSrc="https://pbs.twimg.com/profile_images/1308041397505855490/z9qBAhAy_400x400.jpg"
             title="Tiokeng Samuel"
+            />
+            <Strory
             />
             
         </div>
