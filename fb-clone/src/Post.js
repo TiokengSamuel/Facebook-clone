8d2a2d5f3dac70807ee24078382dbf8bc01764b8
TiokengSamuel <samueledward40@gmail.com>
2020-10-13 21:16:32 +0100

    post

--- fb-clone/src/Post.js
+++ fb-clone/src/Post.js
@@ -1,3 +1,4 @@
+import { Avatar } from '@material-ui/core'
 import React from 'react'
 import './Post.css'
 
@@ -5,7 +6,9 @@ function Post({ profilePic, image, username, timestamp, message}) {
     return (
         <div className="post">
             <div className="post__top">
-                
+                <Avatar src={profilePic}
+                className="post__avatar"
+                />
 
             </div>
         </div>
