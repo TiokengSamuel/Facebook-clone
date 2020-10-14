cb901615237a129d889bb19c4923725cb3e125b1
TiokengSamuel <samueledward40@gmail.com>
2020-10-14 15:19:31 +0100

    name

--- fb-clone/src/StoryReel.js
+++ fb-clone/src/StoryReel.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import './StoryReel.css'
 import Story from './Story'
+import { useStateValue } from './StateProvider';
 
 function StoryReel() {
+    const [{user}, dispatch] = useStateValue();
+
     return (
         <div className="storyReel">
             <Story
             image="https://bucket.mg.co.za/wp-media/2019/06/f5a37fb1-00-yogan-pillay-south-africa-aids-conference-new-infections-innovations-696x445.jpeg"
-            profileSrc="https://pbs.twimg.com/profile_images/1308041397505855490/z9qBAhAy_400x400.jpg"
-            title="Tiokeng Samuel"
+            profileSrc={user.photoURL}
+            title={user.displayName}
             />
             <Story
             image="https://s.abcnews.com/images/Politics/trump-biden-splt-2-gty-ps-200929_1601431396895_hpMain_16x9_992.jpg"
