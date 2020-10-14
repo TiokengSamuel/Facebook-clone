d6157f5db1872a9533d88825c382fc9caf735484
TiokengSamuel <samueledward40@gmail.com>
2020-10-14 08:44:39 +0100

    login

--- fb-clone/src/Login.js
+++ fb-clone/src/Login.js
@@ -5,7 +5,7 @@ function Login() {
     return (
         <div className="login">
             <div className="login__logo">
-                
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo%28202019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png" alt=""/>
             </div>
         </div>
     )
