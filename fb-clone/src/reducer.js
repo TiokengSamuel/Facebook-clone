dcae0ec54453e50806c26ff7498d76f08eb415a6
TiokengSamuel <samueledward40@gmail.com>
2020-10-14 12:40:45 +0100

    red

--- fb-clone/src/reducer.js
+++ fb-clone/src/reducer.js
@@ -4,4 +4,11 @@ export const initialState = {
 
 export const actionTypes = {
     SET_USER: "SET_USER",
+};
+
+const reducer = (state, action) => {
+    console.log(action);
+    switch (action.type) {
+        
+    }
 }
