e4830539385aee2697d732eaa3432e3ea80a8cbb
TiokengSamuel <samueledward40@gmail.com>
2020-10-14 17:39:08 +0100

    post

--- fb-clone/src/Feed.js
+++ fb-clone/src/Feed.js
@@ -19,20 +19,13 @@ function Feed() {
         <div className="feed">
             <StoryReel/>
             <MessageSender/>
-            <Post
-                profilePic="https://pbs.twimg.com/profile_images/1308041397505855490/z9qBAhAy_400x400.jpg"
-                message="Wow it worked"
-                timestamp="This is a timestamp"
-                username="Tiokeng Samuel"
-                image="https://bucket.mg.co.za/wp-media/2019/06/f5a37fb1-00-yogan-pillay-south-africa-aids-conference-new-infections-innovations-696x445.jpeg"
-            />
-            <Post
-                profilePic="https://pbs.twimg.com/profile_images/1308041397505855490/z9qBAhAy_400x400.jpg"
-                message="Hey, Facebook clone looking preety good"
-                timestamp="This is a timestamp"
-                username="Tiokeng Samuel"
-                
-            />
+            {posts.map(post => {
+                <Post
+                    key={post.id}
+                    profilePic={post.profilePic}
+                    
+                />
+            })}
         </div>
     )
 }
