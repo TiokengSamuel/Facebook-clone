97e91120839bd87afe7874f8eba0830da9e3851f
TiokengSamuel <samueledward40@gmail.com>
2020-10-14 14:49:27 +0100

    header

--- fb-clone/src/Header.js
+++ fb-clone/src/Header.js
@@ -48,7 +48,7 @@ function Header() {
             <div className="header__right">
                 <div className="header__info">
                     <Avatar src={user.photoURL}/>
-    <h4>{}</h4>
+                    <h4>{user.displayName}</h4>
                 </div>
 
 
