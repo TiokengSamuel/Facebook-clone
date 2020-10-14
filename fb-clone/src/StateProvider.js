e1496af19795109a253f11b13393f54b246b8b6f
TiokengSamuel <samueledward40@gmail.com>
2020-10-14 12:35:57 +0100

    stat

--- fb-clone/src/StateProvider.js
+++ fb-clone/src/StateProvider.js
@@ -2,9 +2,11 @@ import React, {createContext, useContext, useReducer} from "react"
 
 export const StateContext = createContext();
 
-export const StateProvider = ({ reducer, initialState, childeren })
+export const StateProvider = ({ reducer, initialState, children })
 => (
     <StateContext.Provider value={useReducer(reducer, initialState)}>
-        
+        {children}
     </StateContext.Provider>
 );
+
+export const useStateValue = () => useContext(StateContext);
