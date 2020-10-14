8db8a87617730818d5dab7d9e94d92cc18480cf7
TiokengSamuel <samueledward40@gmail.com>
2020-10-14 05:04:01 +0100

    w

--- fb-clone/src/Widgets.js
+++ fb-clone/src/Widgets.js
@@ -4,10 +4,14 @@ import './Widgets.css'
 function Widgets() {
     return (
         <div className = "widgets">
-           <iframe src="https://web.facebook.com/samuel.tiokeng"
-           width="340"
-           height="100%"
-           style={{ border: "none", overflow: "hidden"}}
+           <iframe 
+            src="https://web.facebook.com/samuel.tiokeng"
+            width="340"
+            height="100%"
+            style={{ border: "none", overflow: "hidden"}}
+            scrolling="no"
+            frameborders="0"
+            allow="encrypted-media">
            </iframe> 
         </div>
     )
