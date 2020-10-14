6449c52ae1e0d31c34037b283e336e23d3a432c6
TiokengSamuel <samueledward40@gmail.com>
2020-10-14 12:49:12 +0100

    ind

--- fb-clone/src/index.js
+++ fb-clone/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { StateProvider } from './StateProvider'
 
 ReactDOM.render(
   <React.StrictMode>
