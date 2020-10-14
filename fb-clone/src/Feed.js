6e86d8283be428fd0ee15bd115324ed47fc821bb
TiokengSamuel <samueledward40@gmail.com>
2020-10-14 16:12:51 +0100

    feed

--- fb-clone/src/Feed.js
+++ fb-clone/src/Feed.js
@@ -8,11 +8,12 @@ import db from "./firebase"
 function Feed() {
     const [posts, setPosts] = useState([]);
 
+    
     useEffect(() => {
         db.collection('posts').onSnapshot(snapshot => {
-            setPosts(snapshot.docs.map(doc => ({})))
+            setPosts(snapshot.docs.map(doc => ({ id: doc.id,data: doc.data() })))
         })
-    }), [])
+    }), []);
 
     return (
         <div className="feed">
