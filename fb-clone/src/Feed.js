1747e13ebad37a5ec7dae209ed1341f3e6f3cd2e
TiokengSamuel <samueledward40@gmail.com>
2020-10-14 15:27:56 +0100

    feed

--- fb-clone/src/Feed.js
+++ fb-clone/src/Feed.js
@@ -5,22 +5,22 @@ import Post from './Post'
 import StoryReel from './StoryReel'
 
 function Feed() {
+    const [{user}, dispatch] = useStateValue();f
+
     return (
         <div className="feed">
             <StoryReel/>
             <MessageSender/>
             <Post
-                profilePic="https://pbs.twimg.com/profile_images/1308041397505855490/z9qBAhAy_400x400.jpg"
+                
                 message="Wow it worked"
-                timestamp="This is a timestamp"
-                username="Tiokeng Samuel"
+                timestamp="This is a timestamp"username="Tiokeng Samuel"
                 image="https://bucket.mg.co.za/wp-media/2019/06/f5a37fb1-00-yogan-pillay-south-africa-aids-conference-new-infections-innovations-696x445.jpeg"
             />
             <Post
-                profilePic="https://pbs.twimg.com/profile_images/1308041397505855490/z9qBAhAy_400x400.jpg"
-                message="Hey, Facebook clone looking preety good"
-                timestamp="This is a timestamp"
-                username="Tiokeng Samuel"
+                
+                message="Facebook clone looking awesome good"
+                timestamp="This is a timestamp"username="Tiokeng Samuel"
                 
             />
         </div>
