7d1857e8074ede2bd93cf1c69979eeba30209741
TiokengSamuel <samueledward40@gmail.com>
2020-10-14 11:08:30 +0100

    f

--- fb-clone/src/firebase.js
+++ fb-clone/src/firebase.js
@@ -1,15 +1,16 @@
 import firebase from "firebase";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAK1JGGp3W-0vtVBmtIIH-yHw_WVxYJS9M",
-    authDomain: "facebook-clone-c2329.firebaseapp.com",
-    databaseURL: "https://facebook-clone-c2329.firebaseio.com",
-    projectId: "facebook-clone-c2329",
-    storageBucket: "facebook-clone-c2329.appspot.com",
-    messagingSenderId: "640552925033",
-    appId: "1:640552925033:web:0de275e8ec5056ff5deff8",
-    measurementId: "G-KQBSRHQLQ3"
-  };
+  apiKey: "AIzaSyAK1JGGp3W-0vtVBmtIIH-yHw_WVxYJS9M",
+  authDomain: "facebook-clone-c2329.firebaseapp.com",
+  databaseURL: "https://facebook-clone-c2329.firebaseio.com",
+  projectId: "facebook-clone-c2329",
+  storageBucket: "facebook-clone-c2329.appspot.com",
+  messagingSenderId: "640552925033",
+  appId: "1:640552925033:web:0de275e8ec5056ff5deff8",
+  measurementId: "G-KQBSRHQLQ3"
+};
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db = firebaseApp.firestore();
