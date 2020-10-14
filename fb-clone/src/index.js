d278bfba018253b9600d1496ee1e617c73f424a8
TiokengSamuel <samueledward40@gmail.com>
2020-10-14 12:46:04 +0100

    state

--- fb-clone/src/index.js
+++ fb-clone/src/index.js
@@ -6,7 +6,9 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState}>
+      <App />
+    </StateProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
