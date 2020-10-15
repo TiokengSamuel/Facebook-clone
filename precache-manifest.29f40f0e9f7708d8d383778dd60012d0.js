e25dff1bd2e83c9c587146d0bcf7757a69785598
Tiokeng Samuel <samueledward40@gmail.com>
2020-10-15 10:45:59 +0100

    Updates

---
+++ precache-manifest.29f40f0e9f7708d8d383778dd60012d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c64945db78d7d3674c704c7e4363c39",
+    "url": "/Facebook-clone/index.html"
+  },
+  {
+    "revision": "1644e667c7d065b08064",
+    "url": "/Facebook-clone/static/css/main.fb15314b.chunk.css"
+  },
+  {
+    "revision": "21095a95787d410a89aa",
+    "url": "/Facebook-clone/static/js/2.45bb128b.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/Facebook-clone/static/js/2.45bb128b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1644e667c7d065b08064",
+    "url": "/Facebook-clone/static/js/main.95f9d9ea.chunk.js"
+  },
+  {
+    "revision": "0d42f339a84d78133706",
+    "url": "/Facebook-clone/static/js/runtime-main.1f071ad4.js"
+  }
+]);
