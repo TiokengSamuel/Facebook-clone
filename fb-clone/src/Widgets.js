f85dbc9a5f4312a2e736b0d1968bc9c18442ae4d
TiokengSamuel <samueledward40@gmail.com>
2020-10-15 07:33:15 +0100

    widget

--- fb-clone/src/Widgets.js
+++ fb-clone/src/Widgets.js
@@ -5,7 +5,7 @@ function Widgets() {
     return (
         <div className = "widgets">
            <iframe 
-            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2F&CleverProgrammerr&tabs=timeline&width=340%height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2F&CleverProgrammerr2F&tabs=timeline&width=340&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
             title="Widget"
             width="340"
             height="100%"
